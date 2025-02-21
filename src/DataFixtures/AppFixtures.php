3ad7d2b5245ed6105e571cdc0c392fff64ae6238
Dorian Feschaud <dorian.feschaud@gmail.com>
2025-02-21 11:07:57 +0100

    user fixtures #2

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,53 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+    private readonly UserPasswordHasherInterface $userPasswordHasher;
+    private readonly Generator $faker;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher) {
+        $this->userPasswordHasher = $userPasswordHasher;
+        $this->faker = $this->createFaker();
+    }
+
+    protected function createFaker()
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        return Factory::create('fr_FR');
+    }
+
+    public function load(ObjectManager $manager):void
+    {
+        $this->loadUsers($manager);
 
         $manager->flush();
+    }
+
+    protected function loadUsers($manager): void
+    {
+        $user = new User();
+        $user->setEmail('superadmin@example.com');
+        $user->setRoles(['ROLE_SUPER_ADMIN']);
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setEmail('admin@example.com');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setEmail('user@example.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
+        $manager->persist($user);
     }
 }
