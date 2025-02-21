98f42d9b21dc65bd60582cec67cea5fb3bcaaf5f
Dorian Feschaud <dorian.feschaud@gmail.com>
2025-02-21 13:06:58 +0100

    products fixture #4

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -27,6 +28,7 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager):void
     {
         $this->loadUsers($manager);
+        $this->loadProducts($manager);
 
         $manager->flush();
     }
@@ -50,5 +52,37 @@ class AppFixtures extends Fixture
         $user->setRoles(['ROLE_USER']);
         $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
         $manager->persist($user);
+    }
+
+    protected function loadProducts($manager, $count = 20): void
+    {
+        for ($i = 0; $i < $count; $i++) {
+            $product = new Product();
+            $product->setName('Product ' . $i);
+            $product->setManufacturer($this->faker->randomElement(['Apple', 'Samsung', 'Xiaomi']));
+            $product->setReleaseDate($this->faker->dateTimeBetween('-10 years'));
+            $product->setPrice($this->faker->randomFloat(2, 300, 2000));
+            $product->setColor($this->faker->colorName());
+            $product->setCapacity($this->faker->randomElement([64, 128, 256, 512]));
+            $height = $this->faker->randomFloat(2, 120, 200);
+            $product->setHeight($height);
+            $width = $this->faker->randomFloat(2, 60, 120);
+            $product->setWidth($width);
+            $product->setThickness($this->faker->randomFloat(2, 0.5, 1.5));
+            $product->setWeight($this->faker->numberBetween(100, 300));
+            $product->setScreen($this->faker->randomElement(['LED', 'OLED', 'QLED']));
+            $product->setScreenHeight($height - $this->faker->randomFloat(2, 0, 30));
+            $product->setScreenWidth($width - $this->faker->randomFloat(2, 0, 15));
+            $product->setScreenResolution($this->faker->numberBetween(300, 500));
+            $product->setBackCamera($this->faker->randomElement(['Simple', 'Double', 'Triple']));
+            $product->setBackCameraResolution($this->faker->numberBetween(40, 60));
+            $product->setFrontCameraResolution($this->faker->numberBetween(10, 20));
+            $product->setProcessor('Processor ' . $this->faker->numberBetween(1, 5));
+            $product->setRam($this->faker->randomElement([8, 12, 16]));
+            $product->setBatteryCapacity($this->faker->numberBetween(3700, 4200));
+            $product->setNetwork($this->faker->randomElement(['5G', '4G']));
+
+            $manager->persist($product);
+        }
     }
 }
