068a6cdc472e3493da7b089a8c196846a27ae55b
Dorian Feschaud <dorian.feschaud@gmail.com>
2025-04-21 21:43:41 +0200

    add customer to product route

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -174,4 +174,39 @@ final class ProductController extends AbstractController{
 
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
+
+    /**
+     * Permet de lier un produit à un ou plusieurs clients.
+     */
+    #[OA\Response(
+        response: 204,
+        description: 'Lier un produit à un ou plusieurs client',
+    )]
+    #[OA\RequestBody(
+        content: new OA\JsonContent(
+            example: '{"customers": [1, 2, 3]}'
+        )
+    )]
+    #[OA\Tag(name: 'Produits')]
+    #[Route('/{id}/addCustomers', name: 'addCustomers', requirements: ['id' => '\d+'], methods: ['POST'])]
+    #[IsGranted('ROLE_SUPER_ADMIN', message: 'Vous ne disposez pas des droits pour lier un produit à un ou plusieurs clients')]
+    public function addCustomers(Product $product, Request $request): JsonResponse
+    {
+        $customers = $this->em->getRepository(Customer::class)->findBy(['id' => $request->toArray()['customers'] ?? []]);
+        foreach ($customers as $customer) {
+            if (!$product->getCustomers()->contains($customer)) {
+                $product->addCustomer($customer);
+                $customer->addProduct($product);
+            }
+        }
+
+        $this->validator->validate($product);
+
+        $this->cache->invalidateTags(['productCache']);
+
+        $this->em->persist($product);
+        $this->em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
 }
