52ee27fa380859a316306c98396ca1363d27edb6
Dorian Feschaud <dorian.feschaud@gmail.com>
2025-04-25 10:10:24 +0200

    add method removeCustomers

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -179,7 +179,7 @@ final class ProductController extends AbstractController{
      * Permet de lier un produit à un ou plusieurs clients.
      */
     #[OA\Response(
-        response: 204,
+        response: 200,
         description: 'Lier un produit à un ou plusieurs client',
     )]
     #[OA\RequestBody(
@@ -207,6 +207,41 @@ final class ProductController extends AbstractController{
         $this->em->persist($product);
         $this->em->flush();
 
-        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+        return new JsonResponse(null, Response::HTTP_OK);
+    }
+
+    /**
+     * Permet de retirer un produit à un ou plusieurs clients.
+     */
+    #[OA\Response(
+        response: 200,
+        description: 'Retirer un produit à un ou plusieurs client',
+    )]
+    #[OA\RequestBody(
+        content: new OA\JsonContent(
+            example: '{"customers": [1, 2, 3]}'
+        )
+    )]
+    #[OA\Tag(name: 'Produits')]
+    #[Route('/{id}/removeCustomers', name: 'removeCustomers', requirements: ['id' => '\d+'], methods: ['POST'])]
+    #[IsGranted('ROLE_SUPER_ADMIN', message: 'Vous ne disposez pas des droits pour retirer un produit à un ou plusieurs clients')]
+    public function removeCustomers(Product $product, Request $request): JsonResponse
+    {
+        $customers = $this->em->getRepository(Customer::class)->findBy(['id' => $request->toArray()['customers'] ?? []]);
+        foreach ($customers as $customer) {
+            if ($product->getCustomers()->contains($customer)) {
+                $product->removeCustomer($customer);
+                $customer->removeProduct($product);
+            }
+        }
+
+        $this->validator->validate($product);
+
+        $this->cache->invalidateTags(['productCache']);
+
+        $this->em->persist($product);
+        $this->em->flush();
+
+        return new JsonResponse(null, Response::HTTP_OK);
     }
 }
